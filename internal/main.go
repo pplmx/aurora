75b1e5afa7ed569a9ca789d348c95a5c49d06e2f
Mystic <215104920@qq.com>
2023-03-06 15:22:16 +0800

    feat(log): add a test for Fields

--- internal/main.go
+++ internal/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/pplmx/aurora/internal/logger"
 	"github.com/pplmx/aurora/test"
+	"go.uber.org/zap"
 )
 
 func main() {
 
 	test.Blockchain()
 
-	logger.DEBUG("hi, this is a debug message")
+	logger.DEBUG("hi, this is a debug message", zap.String("test", "add kv"))
 	logger.INFO("hi, this is a info message")
 	logger.WARN("hi, this is a warn message")
 	logger.ERROR("hi, this is a error message")
