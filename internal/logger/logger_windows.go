78fbf306082de193ba61794047d1dc871769e6ed
Mystic <215104920@qq.com>
2023-03-06 13:51:37 +0800

    feat(log): add zap logger

---
+++ internal/logger/logger_windows.go
@@ -0,0 +1,101 @@
+//go:build windows
+
+package logger
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"syscall"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	CompanyName = "Mystic"
+	AppName     = "aurora"
+)
+
+var (
+	CompanyDir string
+	AppDir     string
+	DebugLog   string
+	InfoLog    string
+	ErrorLog   string
+)
+
+var Logger *zap.Logger
+
+func init() {
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println("Failed to get windows user config dir.")
+		panic(fmt.Sprintf("Failed to get windows user config dir: %v", err))
+	}
+	CompanyDir = dir + "\\" + CompanyName
+	AppDir = CompanyDir + "\\" + AppName
+	DebugLog = AppDir + "\\" + AppName + "-debug.log"
+	InfoLog = AppDir + "\\" + AppName + ".log"
+	ErrorLog = AppDir + "\\" + AppName + "-error.log"
+	err = os.MkdirAll(AppDir, os.ModePerm)
+	if err != nil {
+		fmt.Printf("Failed to create dir %v.\n", AppDir)
+		return
+	}
+	Logger = newZapLogger()
+}
+
+func newZapLogger() *zap.Logger {
+	// First, define our level-handling logic.
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	stdPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.InfoLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.DebugLevel
+	})
+
+	// write to the files or the stdout[stderr]
+	fileDebug := getWriteSyncer(DebugLog)
+	fileStd := getWriteSyncer(InfoLog)
+	fileError := getWriteSyncer(ErrorLog)
+
+	enc := zap.NewProductionEncoderConfig()
+	enc.TimeKey = "time"
+	enc.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000")
+	fileEncoder := zapcore.NewJSONEncoder(enc)
+
+	// Join the outputs, encoders, and level-handling functions into
+	// zapcore.Cores, then tee the four cores together.
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, fileError, highPriority),
+		zapcore.NewCore(fileEncoder, fileStd, stdPriority),
+		zapcore.NewCore(fileEncoder, fileDebug, lowPriority),
+	)
+
+	// From a zapcore.Core, it's easy to construct a Logger.
+	//Open development mode, stack trace
+	caller := zap.AddCaller()
+	//Open file and line number
+	development := zap.Development()
+	fields := zap.Fields(zap.String("app", AppName))
+	logger := zap.New(core, caller, development, fields)
+	defer func(logger *zap.Logger) {
+		err := logger.Sync()
+		if err != nil && !errors.Is(err, syscall.ENOTTY) {
+			panic(err)
+		}
+	}(logger)
+	return logger
+}
+
+func getWriteSyncer(path string) zapcore.WriteSyncer {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
--- internal/main.go
+++ internal/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/pplmx/aurora/test"
+import (
+	"github.com/pplmx/aurora/internal/logger"
+	"github.com/pplmx/aurora/test"
+)
 
 func main() {
 
 	test.Blockchain()
+
+	logger.Logger.Debug("hi, this is a debug message")
+	logger.Logger.Info("hi, this is a info message")
+	logger.Logger.Warn("hi, this is a warn message")
+	logger.Logger.Error("hi, this is a error message")
+	logger.Logger.Fatal("hi, this is a fatal message")
 
 }
