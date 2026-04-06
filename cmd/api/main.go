0ccb231bbcd6c571a87d1630c6346ef59bb00099
Mystvio <215104920@qq.com>
2026-04-06 18:48:13 +0800

    feat: add REST API server with Chi framework
    - Add server configuration
    - Add API middleware (logger, recovery)
    - Add lottery, voting, NFT handlers
    - Add router and main program

---
+++ cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pplmx/aurora/internal/api"
+	"github.com/pplmx/aurora/internal/config"
+	"github.com/pplmx/aurora/internal/logger"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+
+	logger.Init()
+
+	router := api.NewRouter()
+
+	addr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
+
+	go func() {
+		logger.Info().Str("addr", addr).Msg("Starting API server")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatal().Err(err).Msg("Server failed")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logger.Info().Msg("Shutting down server...")
+	if err := server.Close(); err != nil {
+		logger.Error().Err(err).Msg("Server closed with error")
+	}
+}
